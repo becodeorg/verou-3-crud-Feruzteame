60bf2a632dd2ccd6fc4d8b51c9db00580bd29b04
feruzteame <teamefitwi@gmail.com>
2022-03-15 04:11:46 -0700

    create list via form

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ### Steps
 1. make a form
-2. create data base
+2. create database
 3. make connection of database
+4. create element via form
--- crud.php
+++ crud.php
@@ -4,3 +4,9 @@
 $mysqli = new mysqli('localhost', 'root', 'root', 'crud') or
 die(mysqli_error($mysqli));
 
+if(isset($_POST['add'])){
+    $todolist = $_POST['todolist'];
+    $time = $_POST['time'];
+    $mysqli-> query("INSERT INTO todolist (todolist, time) VALUES ('$todolist', '$time')") or
+    die ($mysqli->error);
+}
